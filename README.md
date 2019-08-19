151d347d29823c2416242e3d68e417e0f020ff6b
happysad- <happysad-@users.noreply.github.com>
2019-08-19 23:00:42 +0100

    Initial Commit: Clean repository with a Maven structure and PR template.

---
+++ README.md
@@ -0,0 +1,7 @@
+# TextAdventure
+
+## Contributing
+
+When contributing please do not track IDE generated files.
+
+When contributing, please use the provided pull request template for new pull requests.
---
+++ src/test/java/com/textadventure/MainTest.java
@@ -0,0 +1,11 @@
+package com.textadventure;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        Assert.assertEquals(true, true);
+    }
+}
