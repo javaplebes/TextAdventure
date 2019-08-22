f775e22cdc18cccb5de601d32f75302a82f80e31
Neil Paul <neilcpaul@gmail.com>
2019-08-22 20:24:54 +0100

    Remove unused imports

--- src/main/java/com/textadventure/GameMap.java
+++ src/main/java/com/textadventure/GameMap.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
--- src/main/java/com/textadventure/Main.java
+++ src/main/java/com/textadventure/Main.java
@@ -1,9 +1,6 @@
 package com.textadventure;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 
 @Slf4j
